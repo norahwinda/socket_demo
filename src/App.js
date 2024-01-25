df1bfe73b4d0b0614af98fdec6ceb408514e58df
norahwinda <norahwinda48@gmail.com>
2024-01-25 10:40:44 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { io } from 'socket.io-client';
 
 function App() {
+
+  useEffect(() => {
+    // Connect to the Socket.IO server
+    const socket = io('https://tracking.uxlivinglab.online/socket'); // Use 'http://' or 'https://' depending on your server configuration
+
+    // Event listener for successful connection
+    socket.on('connect', () => {
+      console.log('Connected to Socket.IO server');
+      socket.emit('message', {"user_email":"email","company_id":"65a8bb1f2d73765634fdcaf5",
+      "user_id":"ce7wODqttW",
+      "lat":"10.8130986",
+      "lng":"5.5393232",
+      "link_id":""}); // Send a message to the server
+    });
+
+    // Event listener for receiving messages from the server
+    socket.on('serverMessage', (message) => {
+      console.log('Received message from server:', message);
+      // Update your component state or perform other actions with the received data
+    });
+
+    socket.on('reactMessage', (message) => {
+      console.log('Received message from server:', message);
+      // Update your component state or perform other actions with the received data
+    });
+
+    // Event listener for Socket.IO errors
+    socket.on('error', (error) => {
+      console.error('Socket.IO error:', error);
+      // Handle error state or reconnect logic here
+    });
+
+    // Event listener for Socket.IO disconnection
+    socket.on('disconnect', () => {
+      console.log('Disconnected from Socket.IO server');
+    });
+
+    // Clean up the socket connection when the component unmounts
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
